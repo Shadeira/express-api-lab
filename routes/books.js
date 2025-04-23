7e07e323a2ac284263ab924cff0bb1331133d070
Shadeira <shadeiranes@gmail.com>
2025-04-23 09:43:19 -0400

    POST

--- routes/books.js
+++ routes/books.js
@@ -7,4 +7,12 @@ router.get('/', (req, res) => {
   res.json(books);
 });
 
+// POST /books - add a new book
+router.post('/', (req, res) => {
+    const newBook = req.body;
+    books.push(newBook);
+    res.status(201).json(newBook);
+  });
+  
+
 module.exports = router;
